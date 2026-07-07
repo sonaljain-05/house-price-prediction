39c86d204d454a02e8defce59421bf4f9f015ecb
sonaljain-05 <sonaljjainn@gmail.com>
2026-07-08 00:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,18 @@ st.set_page_config(
     page_icon="🏠",
     layout="centered"
 )
+st.markdown("""
+<style>
+h1{
+    color: #0B6E4F !important;   /* Dark Green */
+    text-align: center;
+    font-size: 48px;
+    font-weight: bold;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
 
 
 def add_bg_from_local(image_file):
